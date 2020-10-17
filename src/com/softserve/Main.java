4f41a78b3e80a7d1cb150c7b1f7285fe3d69a643
Liubomyr Izdepskyi <41821541+Liubomyr17@users.noreply.github.com>
2020-10-17 15:36:01 +0300

    Add files via upload

---
+++ src/com/softserve/Main.java
@@ -0,0 +1,20 @@
+package com.softserve;
+
+import java.util.*;
+
+public class Main {
+        public static void main(String[] args) {
+
+                System.out.println("First level");
+                System.out.println('\t' + "Second level");
+                System.out.println('\t' + "" + '\t' + "Third level");
+
+                System.out.println("First level");
+                System.out.println(" " + "Second level");
+                System.out.println(" " + " " + "Third level");
+
+        }
+}
+
+
+
